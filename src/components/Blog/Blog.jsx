90aa40fcb215aeed6205dc2dc6f2aee06e98831d
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-25 02:37:37 +0600

    4.2q add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,10 +3,21 @@ import React from "react";
 const Blog = () => {
   return (
     <div className="container">
-      <h2>4.1 what is cors?</h2>
+      <h2>4.1 What is cors?</h2>
       <p>
         CORS is a node.js package for providing a Connect/Express middleware
         that can be used to enable CORS with various options.
+      </p>
+      <hr />
+      <h2>
+        4.2 Why are you using firebase? What other options do you have to
+        implement authentication?
+      </h2>
+      <p>
+        I am using firebase for authentication.
+        <br />
+        Other options for implement authentication: Keycloak,Okta,Auth0,Amazon
+        Cognito.
       </p>
     </div>
   );
