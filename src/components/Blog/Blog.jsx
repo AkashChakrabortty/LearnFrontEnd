4be0192fef2749c92a9a02e79885604f317e81c0
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-25 02:44:11 +0600

    4.4q add

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -26,6 +26,18 @@ const Blog = () => {
         if the user is logged in. If not logged in the user is redirected to the
         /login page with the return url passed in the location state property.
       </p>
+      <hr />
+      <h2>4.4 What is Node? How does Node work?</h2>
+      <p>
+        Node. js is a JavaScript runtime environment that achieves low latency
+        and high throughput by taking a “non-blocking” approach to serving
+        requests. <br />
+        Node allows developers to write JavaScript code that runs directly in a
+        computer process itself instead of in a browser. Node can, therefore, be
+        used to write server-side applications with access to the operating
+        system, file system, and everything else required to build
+        fully-functional applications.
+      </p>
     </div>
   );
 };
