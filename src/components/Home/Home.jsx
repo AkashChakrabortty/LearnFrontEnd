fcf8a5b5965366c6fbb4641f5cd73f4adf1e3aab
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-10-26 22:51:35 +0600

    responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container d-flex align-items-center gap-2">
+    <div className="container d-md-flex align-items-center gap-2">
       <div className="left">
         <img
           src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U="
@@ -11,7 +11,9 @@ const Home = () => {
         />
       </div>
       <div className="right">
-        <h2 className="text-primary">Learn front end development</h2>
+        <h2 className="text-primary text-center">
+          Learn front end development
+        </h2>
       </div>
     </div>
   );
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -16,20 +16,24 @@ const Nav = () => {
   return (
     <div>
       <nav>
-        <div className="container d-flex justify-content-between">
-          <div className="left-nav d-flex align-items-center">
+        <div className="container-fluid d-flex justify-content-between gap-2 p-2">
+          <div className="left-nav d-flex align-items-center text-center col-4">
             <Link to="/">
-              <img src="./icons8-developer-96.png" alt="logo" />
+              <img
+                src="./icons8-developer-96.png"
+                alt="logo"
+                className="col-6"
+              />
             </Link>
-            <Link to="/" className="text-decoration-none fs-3">
+            <Link to="/" className="text-decoration-none fs-lg-2">
               LearnFrontEnd
             </Link>
           </div>
-          <div className="right-nav fs-5 d-flex align-items-center">
+          <div className="right-nav fs-lg-3 d-flex align-items-center col-8 justify-content-end">
             <Link to="/course" className="text-decoration-none ">
               Courses
             </Link>
-            <Link to="/faq" className="text-decoration-none mx-3">
+            <Link to="/faq" className="text-decoration-none mx-1 mx-lg-3">
               FAQ
             </Link>
             <Link className="text-decoration-none " to="/blog">
@@ -37,11 +41,10 @@ const Nav = () => {
             </Link>
             <button
               onClick={toggleTheme}
-              className="mx-3 text-primary border-primary"
+              className="mx-lg-3 mx-1 text-primary border-primary"
             >
               Toggle Theme
             </button>
-            {/* <Link className="text-decoration-none mx-3">ToggleTheme</Link> */}
             <Link className="text-decoration-none " to="/login">
               Login
             </Link>
